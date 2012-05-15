d46c8acd1bf6c65b259f270c5b60a27a988030e5
Vitor Calejuri <ondeosfrangosnaotemvez@gmail.com>
2012-05-15 01:27:37 -0300

    Removing media

--- urls.py
+++ urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     
     url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT
-    })
+    #url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+    #    'document_root': settings.MEDIA_ROOT
+    #})
 )
