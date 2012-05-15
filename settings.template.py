3ff6583b5ac0e45637e1867aec5b49a8eda5795a
Vitor Calejuri <ondeosfrangosnaotemvez@gmail.com>
2012-05-15 15:05:04 -0300

    Fixing MEDIA_URL and STATIC_URL paths

--- settings.template.py
+++ settings.template.py
@@ -48,7 +48,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/var/rapp/bancogenital/media/'
+MEDIA_ROOT = '/var/rapp/bancogenital/media/photos/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -59,7 +59,7 @@ MEDIA_URL = 'http://genitalia.me/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/rapp/bancogenital/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,7 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/var/rapp/bancogenital/static",
+    "/var/rapp/bancogenital/media/assets",
 )
 
 # List of finder classes that know how to find static files in
