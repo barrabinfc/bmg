0dffe132f5fe380e3505d66893b1c68f07467ebe
Vitor Calejuri <ondeosfrangosnaotemvez@gmail.com>
2012-05-15 01:04:36 -0300

    Fixing settings

--- settings.template.py
+++ settings.template.py
@@ -48,12 +48,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/vagrant/bancogenital/media/'
+MEDIA_ROOT = '/home/rapp/bancogenital/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://localhost:8080/media/'
+MEDIA_URL = 'http://genitalia.me/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -63,14 +63,14 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/media/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/vagrant/bancogenital/",
+    "/home/rapp/bancogenital/media",
 )
 
 # List of finder classes that know how to find static files in
@@ -163,5 +163,7 @@ LOGGING = {
         },
     }
 }
+
+ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 from app_settings import *
