740abaf7f542df40f4154ea754dcd3c7e788a8ae
Barrabin Fc. ⚑ <barrabin.fc@gmail.com>
2013-03-06 02:33:32 -0300

    Fixing URLs

--- settings_production.py
+++ settings_production.py
@@ -65,14 +65,14 @@ USE_TZ = False
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = "%s/app/media/" % PROJECT_ROOT
-MEDIA_URL = 'http://genitalia.me/media/'
+MEDIA_URL = 'http://capodefusca.genitalia.me/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = "%s/static/" % (PROJECT_ROOT)
-STATIC_URL = 'http://genitalia.me/static/'
+STATIC_URL = 'http://capodefusca.genitalia.me/static/'
 
 STATICFILES_DIRS = (
     '%s/app/media/assets' % (PROJECT_ROOT),
