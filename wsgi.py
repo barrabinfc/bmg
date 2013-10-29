07b778856dc184ad3e54794e119621bc672de7d7
Barrabin Fc. ⚑ <barrabin.fc@gmail.com>
2013-10-30 03:56:38 +0400

    Fix wsgi

--- wsgi.py
+++ wsgi.py
@@ -25,4 +25,4 @@ application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+applications = {'/': application}
