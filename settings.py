3ee9013de1d0e4d31bab0741da7ba595184ca6ba
barrabin.fc <barrabin.fc@gmail.com>
2013-10-29 21:18:46 -0200

    Fixing bancogenital structure

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',                 # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3',                 # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'bancogenital',                      # Or path to database file if using sqlite3.
         'USER':     'bancogenital',                      # Not used with sqlite3.
         'PASSWORD': 'vovozinho',                  # Not used with sqlite3.
