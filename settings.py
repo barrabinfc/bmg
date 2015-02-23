bd444be6ebde5b8fb15bdfecd5c4af8332c5ee4b
Barrabin Fc. ⚑ <barrabin.fc@gmail.com>
2015-02-23 18:32:22 -0300

    By default, use hostname genitalia.me instead of localhost

--- settings.py
+++ settings.py
@@ -64,14 +64,14 @@ USE_TZ = False
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = "%s/media/" % PROJECT_ROOT
-MEDIA_URL = 'http://localhost:8000/media/'
+MEDIA_URL = 'http://genitalia.me/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = "%s/static/" % (PROJECT_ROOT)
-STATIC_URL = 'http://localhost:8000/static/'
+STATIC_URL = 'http://genitalia.me/static/'
 
 STATICFILES_DIRS = (
     '%s/media/assets' % (PROJECT_ROOT),
