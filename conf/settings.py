b759515e43c89adfaaccdcf13f0f5cda85018ba9
Barrabin Fc. ⚑ <barrabin.fc@gmail.com>
2015-12-11 11:14:31 -0200

    Example confs

--- conf/settings.py
+++ conf/settings.py
@@ -23,7 +23,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',                 # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME':     'bancogenital',                      # Or path to database file if using sqlite3.
         'USER':     'bancogenital',                      # Not used with sqlite3.
-        'PASSWORD': 'vovozinho',                  # Not used with sqlite3.
+        'PASSWORD': 'hotlineblaft',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
