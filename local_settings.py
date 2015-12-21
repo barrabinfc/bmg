0148e812087a3405260b6ae235ba788f3a1d3386
Augusto Bennemann <gutobenn@gmail.com>
2015-12-21 13:53:53 -0200

    Update URLS to https

--- local_settings.py
+++ local_settings.py
@@ -74,7 +74,7 @@ USE_TZ = False
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = "%s/media/" % BASE_DIR 
-MEDIA_URL = 'http://genitalia.me/media/'
+MEDIA_URL = 'https://genitalia.me/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
